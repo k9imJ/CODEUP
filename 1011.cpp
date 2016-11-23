312595240a7583f78746aa62aa6465a3d31e5af3
KimJ <ziun79@gmail.com>
2016-11-23 09:33:54 +0900

    Create 1011.cpp

---
+++ 1011.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main()
+ 
+{
+ 
+    char x; //문자(character)를 저장할 수 있도록 x 라는 이름의 변수 준비
+ 
+    scanf("%c", &x); //키보드로 입력되는 값을 문자(character)로 읽어 변수 x에 저장
+ 
+    printf("%c", x); //변수 x에 저장되어 있는 값을 문자(%c)로 출력
+ 
+}
