4dec38ef1f7e2d63e17ec11f92646c107ddbbda5
KimJ <ziun79@gmail.com>
2016-11-23 09:33:21 +0900

    Create 1010.cpp

---
+++ 1010.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    int n;  //정수(integer) 값을 저장할 수 있도록 n 이라는 이름의 변수 준비
+    scanf("%d", &n);  //키보드로 입력되는 값을 10진수(decimal)로 읽어 변수 n에 저장
+    printf("%d",n);  //변수 n에 저장되어있는 값을 10진수(%d)로 출력
+ 
+}
